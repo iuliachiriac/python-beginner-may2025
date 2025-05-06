a664dbfc6d5d99027fcc94f8bd8fbf98faf80da8
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-05-06 13:47:39 +0300

    Add solution for operations with numbers

--- script.py
+++ script.py
@@ -3,4 +3,9 @@ print('Hello world!')
 # Greet person with given name
 name = "Iulia"  # change the value of the name here
 print("Hello " + name + "!")
-print("asdjncsdfndkj")
+
+my_str = "hello"
+multiline_str = """hello
+world
+test
+"""
