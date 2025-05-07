2e579ac9f425b30b1f43a69cabda1c26d72945e7
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-05-07 10:40:27 +0300

    Add solutions for boolean expressions and comparison to None

--- examples/control_flow.py
+++ examples/control_flow.py
@@ -3,6 +3,13 @@ is_cat = True
 
 if is_cat:
     print(f"{name} is a cat")
+    print("is_cat is a truthy value (not necessarily a boolean)")
     print("Still under if")
+else:
+    print(f"{name} is not a cat")
 
 print("Outside if")
+
+if is_cat is True:
+    print(f"{name} is a cat")
+    print("is_cat is a boolean")
