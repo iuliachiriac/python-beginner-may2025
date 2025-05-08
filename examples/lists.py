45136d0be55a7caf35c10f621c9b1663ff63ee71
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-05-08 10:53:19 +0300

    Update lists example with more comments

--- examples/lists.py
+++ examples/lists.py
@@ -1,9 +1,16 @@
 list_of_lists = [[10, 20], [40], [30, 56, 25], [23, 14], [33], [], [40, 22, 47, 39]]
 # renaming inner_list to list will cause shadowing
 for inner_list in list_of_lists:
+    # instead of calculating the length of the list (i),
+    # the `len` function can be used:
+    # i = len(inner_list)
+
     i = 0
     for nr in inner_list:
         i = i + 1
+
+    # `len` function can be used directly in comparison expression
+    # if len(inner_list) >= 2:
     if i >= 2:
         print(inner_list)
 
