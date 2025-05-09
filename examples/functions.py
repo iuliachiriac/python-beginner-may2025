9402e150799268db3e49567f6f3e7517a47da0ac
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-05-09 13:56:46 +0300

    Add import example and solution

--- examples/functions.py
+++ examples/functions.py
@@ -1,24 +1,10 @@
-from string import punctuation
-
-
 def my_function():
     """Prints a greeting message"""
     print('Hello world!')
 
 
 def greet(name: str):  # function with type hints
-    print(len(name))
     print(f"Hello, {name}!")
-
-
-print(my_function)
-help(my_function)
-
-my_function()
-my_function()
-
-greet("Anna")
-# greet(1)
 
 
 def get_words(sentence):
@@ -30,8 +16,20 @@ def get_words(sentence):
     return words
 
 
-print(get_words("When we discussed Python data types, we (purposely) missed a"
-                " big part of their behavior: their methods. "))
-text = ("But, they are associated with objects, which means we need an object "
-        "on which we call them.")
-print(get_words(text))
+print("__name__ in functions.py =", __name__)
+
+if __name__ == "__main__":  # if current module is executed (not imported)
+    print(my_function)
+    help(my_function)
+
+    my_function()
+    my_function()
+
+    greet("Anna")
+    # greet(1)
+
+    print(get_words("When we discussed Python data types, we (purposely) "
+                    "missed a big part of their behavior: their methods. "))
+    text = ("But, they are associated with objects, which means we need an "
+            "object on which we call them.")
+    print(get_words(text))
---
+++ examples/imports.py
@@ -0,0 +1,9 @@
+import functions
+import sys
+import os
+
+print("__name__ in imports.py =", __name__)
+functions.greet("Julie")
+
+print(sys.path)
+print(os.getcwd())
