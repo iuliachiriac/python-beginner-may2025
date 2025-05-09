2e63b6f554563fa2e13c6b5cde794d475b458739
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-05-09 09:36:31 +0300

    Add solutions for str methods

--- examples/functions.py
+++ examples/functions.py
@@ -1,3 +1,6 @@
+from string import punctuation
+
+
 def my_function():
     """Prints a greeting message"""
     print('Hello world!')
@@ -16,3 +19,19 @@ my_function()
 
 greet("Anna")
 # greet(1)
+
+
+def get_words(sentence):
+    punctuation_marks = ",.!?):"
+    for char in punctuation_marks:
+        sentence = sentence.replace(char, "")
+    sentence = sentence.lower()
+    words = sentence.split()
+    return words
+
+
+print(get_words("When we discussed Python data types, we (purposely) missed a"
+                " big part of their behavior: their methods. "))
+text = ("But, they are associated with objects, which means we need an object "
+        "on which we call them.")
+print(get_words(text))
